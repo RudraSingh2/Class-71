a0c1ade2f45bf4f4784c84fbe1d6b0f1c1e48933
Rudra <rudra2singh10@gmail.com>
2021-07-02 15:37:23 +0530

    Willy App

--- screens/BookTransactionScreen.js
+++ screens/BookTransactionScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View,TouchableOpacity   } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity ,TextInput ,Image } from 'react-native';
 import * as Permissions from 'expo-permissions';
 
 import {BarCodeScanner} from "expo-barcode-scanner";
@@ -13,11 +13,13 @@ export default class BookTransactionScreen extends Component {
       hasCameraPermissions:null,
       scanned:false,
       scannedData : '',
-      buttonState:'normal'          
+      buttonState:'normal' ,
+      scannedBookID :'',
+      scannedStudentID:''         
     }
   }
 
-  getCameraPermissions = async ()=>{
+  getCameraPermissions = async (id)=>{
     console.log("getCameraPermissions ");
     // returns a value granted if permission is given
       const {status} = Permissions.askAsync(Permissions.CAMERA);
@@ -27,44 +29,71 @@ export default class BookTransactionScreen extends Component {
           status === "granted" is false when user has not granted the permission
         */
         hasCameraPermissions: status === "granted",
-        buttonState:'clicked',
+        buttonState:id,
         scanned: false
       })
       console.log(this.state.hasCameraPermissions);
   }
   handleBarCodeScanned = async({type,data})=>{
-    this.setState({
-      scanned:true,
-      scannedData : data,
-      buttonState:'normal'
-    })
+    const {buttonState} = this.state
+    if (buttonState==='bookID') {
+      this.setState({
+        scanned:true,
+        scannedBookID : data,
+        buttonState:'normal'
+      })
+    } else if(buttonState==='StudentID'){
+      this.setState({
+        scanned:true,
+        scannedStudentID : data,
+        buttonState:'normal'
+      })
+    }
+    
   }
   
   render(){
     const hasCameraPermissions = this.state.hasCameraPermissions;
     const scanned = this.state.scanned;
     const buttonState = this.state.buttonState
-    if(buttonState === "clicked" && hasCameraPermissions){
+    console.log(buttonState);
+    if(buttonState !== "normal" && hasCameraPermissions){
       return(
         <BarCodeScanner
-        onBarCodeScanned = {scanned?undefined:this.handleBarCodeScanned}
+        onBarCodeScanned = {scanned?undefined:this.handleBarCodeScanned}  
         style={StyleSheet.absoluteFillObject}
         />
         )
     }else {
-      return (  
+      console.log("I am in else ");
+      return (          
         <View style={styles.container} >
-          <Text style={styles.displayText}>
-           {hasCameraPermissions === true ? this.state.scannedData : "Request Camera Permission"}
-          </Text>  
-          <TouchableOpacity 
-            style={styles.scanButton}
-            onPress = {()=>{
-              this.getCameraPermissions();
-            }}
-          >
-           <Text style={styles.buttonText}>Scan QR Code</Text> 
-          </TouchableOpacity>   
+          <View>
+            <Image source = {require('../assets/booklogo.jpg')} style={{width:200,height:200}}/>
+            <Text style={{textAlign:"center",fontSize:30}}>Wily</Text>
+          </View>
+          <View style = {styles.inputView}>
+            <TextInput style={styles.inputBox} placeholder="Book Id" value= {this.state.scannedBookID}/>
+            <TouchableOpacity 
+              style={styles.scanButton}
+              onPress = {()=>{
+                this.getCameraPermissions('bookID');
+              }}
+            >
+            <Text style={styles.buttonText}>Scan Book Id</Text> 
+            </TouchableOpacity>  
+          </View>  
+          <View style = {styles.inputView}>            
+            <TextInput  style={styles.inputBox} placeholder="Student Id" value= {this.state.scannedStudentID}/>
+            <TouchableOpacity 
+              style={styles.scanButton}
+              onPress = {()=>{
+                this.getCameraPermissions('StudentID');
+              }}
+            >
+            <Text style={styles.buttonText}>Scan Student Id</Text> 
+            </TouchableOpacity>  
+          </View>     
         </View>
       );
     }
@@ -72,7 +101,7 @@ export default class BookTransactionScreen extends Component {
   }
   
 }
-                                                                     
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -89,6 +118,25 @@ const styles = StyleSheet.create({
     margin: 10
   },
   buttonText:{
-    fontSize: 20,
+    fontSize: 15,
+    textAlign: 'center',
+    marginTop: 10
+  },
+  inputView:{
+    flexDirection: 'row',
+    margin: 20
+  },
+  inputBox:{
+    width: 200,
+    height: 40,
+    borderWidth: 1.5,
+    borderRightWidth: 0,
+    fontSize: 20
+  },
+  scanButton:{
+    backgroundColor: '#66BB6A',
+    width: 75,
+    borderWidth: 1.5,
+    borderLeftWidth: 0
   }
 });
